545908bacb01f3b3a8eac0cb7fb23a088d73f453
samlimby <129619263+samlimby@users.noreply.github.com>
2024-09-07 09:54:00 +0100

    conversion functionality complete

--- unit_converter/script.js
+++ unit_converter/script.js
@@ -1,12 +1,9 @@
-// initialize the input value
-// initialize the conversion formula for meter-feet, liters-gallons and kg-pounds
 // declare the localStorage requirements
-// create functions for each conversion
 // initialize DOM interaction for each of the three conversion to be more optimal for performance
-// function for rendering out the value using individual functions and modify the inner.HTML
 
 const inputA = document.getElementById("digit-input");
 const primaryButton = document.getElementById("primary-button");
+const modeButton = document.getElementById("mode-button");
 let lengthText = document.getElementById("length-text");
 let volumeText = document.getElementById("volume-text");
 let massText = document.getElementById("mass-text");
@@ -18,13 +15,30 @@ const gallonConversion = 1 * 3.785;
 const kiloConversion = 1 * 2.205;
 const poundConversion = 1 / 2.205;
 
+localStorage.setItem("value", "input");
+
 inputA.addEventListener("input", function(){
     console.log("input typed in");
     lengthTextConversion();
+    volumeTextConversion();
+    massTextConversion();
 });
 
 function lengthTextConversion() {
     lengthText.innerHTML = `
-        <p class="body-text" id="length-text">${inputA.value} meters = ${inputA.value * meterConversion} feet | ${inputA.value} feet = ${inputA.value * feetConversion} meters </p>
+        <p class="body-text" id="length-text">${inputA.value} meters = ${inputA.value * meterConversion.toFixed(2)} feet | ${inputA.value} feet = ${inputA.value * feetConversion.toFixed(2)} meters </p>
     `;
 };
+
+function volumeTextConversion() {
+    volumeText.innerHTML = `
+        <p class="body-text" id="volume-text">${inputA.value} liters = ${inputA.value * literConversion.toFixed(2)} gallons | ${inputA.value} gallons = ${inputA.value * gallonConversion.toFixed(2)} liters </p>
+    `;
+};
+
+function massTextConversion() {
+    massText.innerHTML = `
+        <p class="body-text" id="mass-text">${inputA.value} kilos = ${inputA.value * kiloConversion.toFixed(2)} pounds | ${inputA.value} pounds = ${inputA.value * poundConversion.toFixed(2)} kilos </p>
+    `;
+};
+
